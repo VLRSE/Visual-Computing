54319e80343e1f110a6739dbd37e44f425e06a06
Vilrose Daquiado <vcdaquiado@gmail.com>
2025-10-19 19:54:58 +0200

    Aufgabe 1.3 Initialen "V" - Vertices für "V" geändert -Indizes für "V" geändert

--- src/Game/Scene.cpp
+++ src/Game/Scene.cpp
@@ -19,16 +19,39 @@ bool Scene::init()
 		m_shader = m_assets.getShaderProgram("shader");
         m_shader->use();
 
-		//2D + Farbe (xyrgb)
-        float vertices[] = {-0.5, -0.5, 0.0, 0.0, 1.0,
-                            0.5, -0.5, 0.0, 0.0, 1.0,
-                            0.5, 0.5, 0.0, 1.0, 0.0,
-                            0.0, 1.0, 1.0, 0.0, 0.0,
-                            -0.5, 0.5, 0.0, 1.0, 0.0};
+			//2D + Farbe (xyrgb)
+        float vertices[] = {
+        	//----------V----------
 
-        int indices[] = {0, 1, 2,
-                         0, 2, 4,
-                         4, 2, 3};
+        	-0.75, 0.8, 0.0, 0.0, 1.0, //0
+            -1.0, 0.8, 0.0, 0.0, 1.0, //1
+            -0.8, 0.0, 0.0, 1.0, 0.0, //2
+            -0.5, -0.5, 1.0, 0.0, 0.0, //3
+            -0.6, -0.8, 0.0, 1.0, 0.0, //4
+	        -0.4, -0.8, 0.0, 1.0, 0.0, //5
+        	-0.2, 0.0, 0.0, 1.0, 0.0, //6
+        	0.0, 0.8, 0.0, 1.0, 0.0, //7
+        	-0.25, 0.8, 0.0, 1.0, 0.0, //8
+
+
+        	//----------D----------
+
+
+
+        };
+
+        int indices[] = {
+
+        	//----------Left----------
+        	0, 1, 2,
+            2, 3, 0,
+            3, 2, 4,
+        	4, 5 , 3,
+        	3, 5, 6,
+        	6, 7, 8,
+        	8,3,6
+        };
+
 
 		/*
 		 * ************
@@ -92,7 +115,7 @@ void Scene::render(float dt)
 
 	//b.Elemente Zeichen (render call)
 	//COUNT: jedes Dreieck hat 3 Indizes
-	glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES, (7 * 3) , GL_UNSIGNED_INT, 0);
 
 	//c. Optionales Lösen der Bindung, um versehentliche Änderungen am VAO zu vermeiden
 	glBindVertexArray(0);
