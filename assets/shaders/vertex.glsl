b75147e55d15bd119bfbe3f00192b4e94548a145
Vilrose Daquiado <vcdaquiado@gmail.com>
2025-10-29 17:10:03 +0100

    vertex.glsl modified for 3D

--- assets/shaders/vertex.glsl
+++ assets/shaders/vertex.glsl
@@ -1,12 +1,16 @@
 #version 330 core
-
-layout (location = 0) in vec2 pos;
+//layout ist Attribut Pointer
+//location ist vertex position
+//in ist input
+layout (location = 0) in vec3 pos;
 layout (location = 1) in vec3 colorRGB;
 
 out vec3 colorVS;
+uniform mat4 model; // 4-Dimensional Matrix
 
 void main(){
     colorVS = colorRGB;
-    gl_Position = vec4(pos.x, pos.y, 0.0, 1.0);
+    gl_Position = model * vec4(pos, 1.0);
+
 
 }
