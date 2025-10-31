25cbf32bbc0b63bdc6044fcf4fd839ee04c2d323
Vilrose Daquiado <vcdaquiado@gmail.com>
2025-10-31 23:00:36 +0100

    -old vertices data deleted -Vaariablen vaoID und vboID in die Scene.h Datei verschoben -set Background color for clearing -Reset Buffers before drwaing -count becomes dynamic -Einheitsmatrix erzeugt -model um 2 Achsen rotiert

--- src/Game/Scene.cpp
+++ src/Game/Scene.cpp
@@ -19,25 +19,6 @@ bool Scene::init()
 		m_shader = m_assets.getShaderProgram("shader");
         m_shader->use();
 
-			//3D + Farbe (xyzrgb)
-        float vertices[] = {
-        	-0.5, -0.5, 1.0, 0.0, 0.0, 1.0,
-			0.5, -0.5,  1.0, 0.0, 0.0, 1.0,
-			0.5, 0.5, 1.0, 0.0, 1.0, 0.0,
-			0.0, 1.0, 1.0, 1.0, 0.0, 0.0,
-			-0.5, 0.5, 1.0, 0.0, 1.0, 0.0
-
-        };
-
-        int indices[] = {
-        	0, 1, 2,
-			0, 2, 4,
-			4, 2, 3
-
-        };
-
-
-
 
 		/*
 		 * ************
@@ -46,7 +27,7 @@ bool Scene::init()
 		 */
 
 		//VBO Erzeugen
-		GLuint vaoID, vboID;
+
 
 
 		transform();
@@ -76,7 +57,7 @@ bool Scene::init()
 		glEnableVertexAttribArray(1);
 
 		//d.)Indexbuffers erstellen und binden.
-		GLuint iboID;
+
 		glGenBuffers(1, &iboID);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboID);
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeInd), cubeInd, GL_STATIC_DRAW);
@@ -106,12 +87,20 @@ void Scene::render(float dt)
     * ************
     */
 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Reset Buffers before drawing
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); //background color black
+
+	//Schick model matrix zu shader
+	m_shader->setUniform("model", cubeTrans->getMatrix(),false);
+
 	//a. VAO Binden.
 	glBindVertexArray( vaoID);
 
 	//b.Elemente Zeichen (render call)
 	//COUNT: jedes Dreieck hat 3 Indizes
-	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES, sizeof(cubeInd)/sizeof(float), GL_UNSIGNED_INT, 0);
+
+
 
 	//c. Optionales Lösen der Bindung, um versehentliche Änderungen am VAO zu vermeiden
 	glBindVertexArray(0);
@@ -131,7 +120,13 @@ OpenGLWindow * Scene::getWindow()
 
 void Scene::transform()
 {
-	auto cubePosition = glm::vec3(0.0f, 0.0f, 0.0f);
+	auto cubePosition = glm::vec3(0.0f, 0.0f, -0.3f);
+
+	//Einheits Matrix für die Rotation 2 Achsen
+	cubeTrans = new Transform();
+
+	//Rotation um 45 Grad Y-Achse und 30 Graf auf X-Achse
+	cubeTrans->rotate(glm::vec3(0.5f, glm::radians(45.0f) , 0.0f));
 }
 
 void Scene::onKey(Key key, Action action, Modifier modifier)
