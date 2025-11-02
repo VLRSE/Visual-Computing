de824d5bfdce55a4955c728a10bdfaa24360839a
Vilrose Daquiado <vilrose.daquiado@stud.h-da.de>
2025-11-02 15:50:05 +0100

    -Neue Uniform im Fragment-Shader hinzugefügt

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -5,9 +5,12 @@ in vec3 colorVS;
 //fragment shader output
 out vec4 color;
 
+uniform float time; //neue Uniform für Animation
 
 void main(){
 
+    float intensity = 0.5 + 0.5 * sin(time); //Wert zwischen 0.0 und 1.0
+    vec3 animatedColor = colorVS * intensity;
     color = vec4(colorVS.x, colorVS.y, colorVS.z, 1.0);
 
 }
