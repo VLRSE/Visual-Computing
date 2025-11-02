fa2294380ce1386e6b6fcf56c4fea5c401b35962
Vilrose Daquiado <vilrose.daquiado@stud.h-da.de>
2025-11-02 15:10:26 +0100

    -Rotation für Beine mit "schwenken" Gehanimation hinzugefügt

--- src/Game/Scene.cpp
+++ src/Game/Scene.cpp
@@ -97,10 +97,28 @@ void Scene::render(float dt)
 
 	//continuous rotation
 	//Rotation um 45 Grad Y-Achse und 30 Graf auf X-Achse
-	rumpf.rotate(glm::vec3(0.2f * dt, glm::radians(60.0f) * dt  , 0.0f));
+	//rumpf.rotate(glm::vec3(0.2f * dt, glm::radians(60.0f) * dt  , 0.0f));
 
 	//a. VAO Binden.
 	glBindVertexArray( vaoID);
+
+	m_time += dt;
+
+	//einfache hin-und-zurück Bewegung mit sin wave
+	float bewegungGeschwindigkeit= 2.0f;
+	float bewegungAmplitude = 0.3f;
+
+	//Rotation Beine
+	float beinAngle = sin(m_time * bewegungGeschwindigkeit) * glm::radians(30.0f) * dt;
+	glm::vec3 hüftePoint = glm::vec3(0.0f, -0.8f, 0.0f);
+	linkesBein.rotateAroundPoint(hüftePoint,  glm::vec3(beinAngle , 0.0f, 0.0f));
+	rechtesBein.rotateAroundPoint(hüftePoint,  glm::vec3(-beinAngle , 0.0f, 0.0f));
+
+	//Rotation obereArme
+	float obereArmAngle = sin(m_time * bewegungGeschwindigkeit) * glm::radians(20.0f) * dt;
+	glm::vec3 obereArmePoint = glm::vec3(0.0f, 0.7f, 0.0f);
+	linkesBein.rotateAroundPoint(obereArmePoint,  glm::vec3(obereArmAngle , 0.0f, 0.0f));
+	rechtesBein.rotateAroundPoint(hüftePoint,  glm::vec3(-beinAngle , 0.0f, 0.0f));
 
 	/***
 	 **Körperteile rendern
@@ -124,6 +142,8 @@ void Scene::render(float dt)
 	glm::mat4 rechtesBeinArmMatrix = rumpf.getMatrix() * beinGruppe.getMatrix() ;
 	transformBodyPart(linkesBein , rechtesBeinArmMatrix);
 	transformBodyPart(rechtesBein , rechtesBeinArmMatrix);
+
+
 
 
 
@@ -159,6 +179,7 @@ void Scene::transform()
 	*/
 	rumpf = Transform();
 	rumpf.scale(glm::vec3(0.5f, 0.8f, 0.2f));
+	rumpf.rotate(glm::vec3(0.1f , glm::radians(60.0f)   , 0.0f));
 
 	/***
 	 *Kopf Transformation
@@ -220,11 +241,6 @@ void Scene::transform()
 
 	rechtesBein = Transform();
 	rechtesBein.translate(glm::vec3(0.8f, 0.0f, 0.0f));
-
-
-
-
-
 
 }
 
