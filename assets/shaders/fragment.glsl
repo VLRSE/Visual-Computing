49f48a39ee74d9a91336d4ed934a415c4b92a04c
Vilrose Daquiado <vilrose.daquiado@stud.h-da.de>
2025-11-02 15:52:43 +0100

    -fragment Shader wurde noch einenn Kommentar für "time" hinzugefügt -und animatedCcolor in color variable eingesetzt

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -9,9 +9,10 @@ uniform float time; //neue Uniform für Animation
 
 void main(){
 
+    //time wird pro Frame von der CPU-Seite gesetzt
     float intensity = 0.5 + 0.5 * sin(time); //Wert zwischen 0.0 und 1.0
     vec3 animatedColor = colorVS * intensity;
-    color = vec4(colorVS.x, colorVS.y, colorVS.z, 1.0);
+    color = vec4(animatedColor, 1.0);
 
 }
 
