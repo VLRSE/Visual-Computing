d778a287e82fdec95e50b704d29bca01b8b438d2
Vilrose Daquiado <vilrose.daquiado@stud.h-da.de>
2025-11-02 21:41:07 +0100

    -fragment glsl file updated with uniform variable "time" added and the animated color

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -12,9 +12,9 @@ void main(){
     //time wird pro Frame von der CPU-Seite gesetzt
 
       // Farbverlauf basierend auf Zeit
-        float r = abs(sin(time * 0.8));
+        float r = abs(sin(time * 0.5));
         float g = abs(sin(time * 0.6 + 2.0));
-        float b = abs(sin(time * 1.0 + 4.0));
+        float b = abs(sin(time * 0.5 + 4.0));
 
     float intensity = 0.8 + 0.5 * sin(time); //Wert zwischen 0.0 und 1.0
     vec3 animatedColor = vec3(r, g, b) * colorVS ;
