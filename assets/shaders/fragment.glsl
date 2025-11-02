0ad7614f1cec518984e376b8a31f04972bda8a27
Vilrose Daquiado <vilrose.daquiado@stud.h-da.de>
2025-11-02 16:12:19 +0100

    -fragment shader die Farbintensität geändert

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -10,8 +10,14 @@ uniform float time; //neue Uniform für Animation
 void main(){
 
     //time wird pro Frame von der CPU-Seite gesetzt
-    float intensity = 0.5 + 0.5 * sin(time); //Wert zwischen 0.0 und 1.0
-    vec3 animatedColor = colorVS * intensity;
+
+      // Farbverlauf basierend auf Zeit
+        float r = abs(sin(time * 0.8));
+        float g = abs(sin(time * 0.6 + 2.0));
+        float b = abs(sin(time * 1.0 + 4.0));
+
+    float intensity = 0.8 + 0.5 * sin(time); //Wert zwischen 0.0 und 1.0
+    vec3 animatedColor = vec3(r, g, b) * colorVS ;
     color = vec4(animatedColor, 1.0);
 
 }
