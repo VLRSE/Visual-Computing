7db524233d19da1a668716bb43ec44487b33181d
Vilrose Daquiado <vilrose.daquiado@stud.h-da.de>
2025-11-13 18:49:27 +0100

    Stop tracking generated pkg-config (.pc) files

--- cmake-build-debug/libs/glm-master/test/ext/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/marti/OneDrive/Dokumente/Vilrose_HDA/Visual Computing/7WiSe25_26/Praktikum/Prak1/VC_OpenGL_Praktikum_Framework/OpenGL_Praktikum/libs/glm-master/test/ext
-# Build directory: C:/Users/marti/OneDrive/Dokumente/Vilrose_HDA/Visual Computing/7WiSe25_26/Praktikum/Prak1/VC_OpenGL_Praktikum_Framework/OpenGL_Praktikum/cmake-build-debug/libs/glm-master/test/ext
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
