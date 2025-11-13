aaa086a2a61162f394f9c683bcfc4a5fe7abb61f
Vilrose Daquiado <vilrose.daquiado@stud.h-da.de>
2025-11-13 20:08:05 +0100

    Ignore glew.pc and other build artifacts

--- cmake-build-debug/libs/glm-master/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/marti/OneDrive/Dokumente/Vilrose_HDA/Visual Computing/7WiSe25_26/Praktikum/Prak1/VC_OpenGL_Praktikum_Framework/OpenGL_Praktikum/libs/glm-master
-# Build directory: C:/Users/marti/OneDrive/Dokumente/Vilrose_HDA/Visual Computing/7WiSe25_26/Praktikum/Prak1/VC_OpenGL_Praktikum_Framework/OpenGL_Praktikum/cmake-build-debug/libs/glm-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
-subdirs("test")
